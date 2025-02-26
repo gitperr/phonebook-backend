574da2e9875370fe611c39473ea9d1e72bf2c97c
Alper Polat <gitperr@gmail.com>
2025-02-26 20:17:44 +0200

    Remove undefined message

--- models/person.js
+++ models/person.js
@@ -9,7 +9,7 @@ console.log('connecting to MongoDB at url:', url)
 mongoose.connect(url)
 
   .then(result => {
-    console.log('connected to MongoDB:', result.message)
+    console.log('connected to MongoDB')
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
