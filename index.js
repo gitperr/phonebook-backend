905dfa80f76415ac0afc58030ec58e000c05f8ff
Alper Polat <gitperr@gmail.com>
2025-02-26 20:27:02 +0200

    Add Phonebook database, step 5 (3.17)

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))  
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
