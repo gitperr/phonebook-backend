60d80c6c78bc77a258e88bcca509a8494c002e0a
Alper Polat <gitperr@gmail.com>
2025-02-26 15:15:50 +0200

    Add Command-line database (exercise 3.12)

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const mongoUrl = `mongodb+srv://gitperr:${password}@cluster0.te2mq.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(mongoUrl)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+if (!personName && !personNumber) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+  return
+}
+
+const person = new Person({
+  name: personName,
+  number: personNumber
+})
+
+person.save().then(result => {
+  console.log(`added ${result.name} number ${result.number} to phonebook`)
+  mongoose.connection.close()
+})
