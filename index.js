0bd8c611326e03276cae187a52257b22a5defc1f
Alper Polat <gitperr@gmail.com>
2025-02-26 20:09:25 +0200

    Add Phonebook database, step 3 (3.15)

--- index.js
+++ index.js
@@ -70,10 +70,12 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error));
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(person => person.id !== request.params.id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
